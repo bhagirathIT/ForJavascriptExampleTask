2b49069de295ce2b7bc6bf5d94c217ce9ac0d3df
Bhagirath Makvana <102416901+bhagirathIT@users.noreply.github.com>
2023-09-25 11:31:44 +0000

    variable file commit

---
+++ 2Variable/1test.js
@@ -0,0 +1,6 @@
+const accountId = 180697;
+let accountEmail = "bhagirath@gmail.com";
+var accountPassword = "123456";
+accountCity = "junagadh";
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
